7026525758584c39f4b207de986ebf70ac625ad6
NesaKrasniqi <nk194@bgt.school>
2025-07-01 11:11:20 +0200

    Update AfternoonTask1.c

--- Day1/AfternoonTask1.c
+++ Day1/AfternoonTask1.c
@@ -1,1 +1,31 @@
+/*  
 
+   Program to print my full name with formatting. 
+
+*/ 
+
+  
+
+#include <stdio.h>  // For printf 
+
+  
+
+int main() { 
+
+    // Print a line with a newline 
+
+    printf("My name is:\n"); 
+
+     
+
+    // Print the name with a tab 
+
+    printf("\tNesa Krasniqi\n"); 
+
+     
+
+    // End program 
+
+    return 0; 
+
+} 
